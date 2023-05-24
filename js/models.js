8d2ab1886de71612562a5e46ec9893ec81b918a1
Moez <moezdurrani1@gmail.com>
2023-05-24 04:06:09 -0500

    files copied

---
+++ js/models.js
@@ -0,0 +1,34 @@
+function fun1(){
+    document.getElementById("bigA").src = 'suspension/index.html';
+    // document.getElementById("detail").innerHTML = "Suspension";
+}
+
+function fun2(){
+    document.getElementById("bigA").src = 'truck/index.html';
+    // document.getElementById("detail").innerHTML = "Tesla Cyber Truck";
+}
+
+function fun3(){
+    document.getElementById("bigA").src = 'board/index.html';
+    // document.getElementById("detail").innerHTML = "Power Genrating Skateboard";
+}
+
+function fun4(){
+    document.getElementById("bigA").src = 'chess/index.html';
+    // document.getElementById("detail").innerHTML = "Chess";
+}
+
+function fun5(){
+    document.getElementById("bigA").src = 'wheelBarrow/index.html';
+    // document.getElementById("detail").innerHTML = "Wheel Barrow";
+}
+
+function fun6(){
+    document.getElementById("bigA").src = 'spinner/index.html';
+    // document.getElementById("detail").innerHTML = "Fidget Spinner";
+}
+
+function fun7(){
+    document.getElementById("bigA").src = 'ninja/index.html';
+    // document.getElementById("detail").innerHTML = "Ninja Blade";
+}
